69427e1f2a51d496f6b5bcbb08b2774ced94da86
Giliord Mpouo <giliordmpouo@gmail.com>
2025-08-23 22:44:20 +0200

    dddd

--- JS/script.js
+++ JS/script.js
@@ -3,10 +3,10 @@ const searchsInputs = document.querySelectorAll(".searchInput");
 
 searchsInputs.forEach(searchInput => {
 
-    searchInput.addEventListener("click", () => {
+    searchInput.addEventListener("focus", () => {
         document.body.style.backgroundColor = "red"
 
-        console.log("c'est cliqué");
+        console.log("c'est en focus");
         
     })
     searchInput.addEventListener("input", () => {
