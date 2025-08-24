43b7364ab0f052b70ce29b40311ffa999c657308
Giliord Mpouo <giliordmpouo@gmail.com>
2025-08-24 21:53:37 +0200

    Test - style sans scale pour les descriptions

--- JS/script.js
+++ JS/script.js
@@ -4,7 +4,7 @@ const searchsInputs = document.querySelectorAll(".searchInput");
 searchsInputs.forEach(searchInput => {
 
     searchInput.addEventListener("focus", () => {
-        document.body.style.backgroundColor = "blue"
+        document.body.style.backgroundColor = "black"
 
         console.log("c'est en focus");
         
